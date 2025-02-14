aa727b46161dc4b4ca28a2aa4913609cdb0edced
yeeun234 <wooyeeun2@gmail.com>
2025-02-14 11:11:04 +0900

    object

--- 06/06.js
+++ 06/06.js
@@ -26,7 +26,7 @@ function lotto() {
 
   
     let spanTgs  = arrLotto.map(item => 
-        item == '+' ? `<span di='spplus'>${item}</span>`
+        item == '+' ? `<span id='spplus'>${item}</span>`
                     :`<span class='sp${Math.floor(item/10)}'>${item}</span>`)
         
     //10으로 나누어서 몫이0,1,2,3이 되니까 1~9는sp0,10~19는sp1,20~29는sp2 이런식으로 class가 다르게 적용됨. 
